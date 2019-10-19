a0e2ab5fdb7ee3018af32167f0c8cc33fa67a14a
Johnathan Bressette <bressetteprogramming@gmail.com>
2019-10-19 19:53:08 -0400

    Add generateItems to generate weight and cost
    The function generateItems was added to generate the weight and cost
    of each item that will be used in the knapsack problem. The function
    takes in the cost and weight arrays alongside the number of items and
    generates random values for the weight and cost of each item. A
    printArray function was also added to print the arrays to test that
    the generateItems function works.
    
    The generateItems function allows for random elements to be generated
    for usage in the knapsack problem and the printArray function will allow
    for the values in every item to be displayed to make sure that the
    algorithms works correctly.

--- 0-1knapsackProblem.c
+++ 0-1knapsackProblem.c
@@ -1,1 +1,37 @@
+#include "stdlib.h"
+#include "stdio.h"
+#include "time.h"
 
+int generateItems(int *cost, int *weight, int numItems)
+{
+    for(int i = 0; i < numItems; i++)
+    {
+        cost[i] = rand() % 100 + 1;
+        weight[i] = rand() % 20 + 1;
+    }
+}
+
+printArray(int *array, int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+
+int main()
+{
+    srand(time(0));
+    int numItems = 10;
+    int *cost, *weight;
+    cost = malloc(sizeof(int) * numItems);
+    weight = malloc(sizeof(int) * numItems);
+
+    generateItems(cost, weight, numItems);
+    printArray(cost, numItems);
+    printArray(weight, numItems);
+
+    return 0;
+}
