a5d4a2f95e70574be32348656fb74bb38c9fd126
Johnathan Bressette <bressetteprogramming@gmail.com>
2019-10-20 10:35:29 -0400

    Add mergeSort to sort cost/weight ratios
    The function mergeSort was added that sorts an array given an array, a
    starting index and a final index.
    
    Sorting an array is useful for sorting the cost/weight ratio of items
    so that the items with the best cost/weight ratios are added first.

--- 0-1knapsackProblem.c
+++ 0-1knapsackProblem.c
@@ -11,7 +11,7 @@ int generateItems(int *cost, int *weight, int numItems)
     }
 }
 
-printArray(int *array, int size)
+void printArray(int *array, int size)
 {
     for(int i = 0; i < size; i++)
     {
@@ -19,6 +19,95 @@ printArray(int *array, int size)
     }
     printf("\n");
 }
+
+int min(int *array, int size)
+{
+    int minimum = array[0];
+
+    for(int i = 1; i < size; i++)
+    {
+        if(array[i] < minimum)
+            minimum = array[i];
+    }
+
+    return minimum;
+}
+
+
+void merge(int *array, int start, int end)
+{
+    int mid = (start+end)/2;
+
+    int i = start;
+    int j = mid + 1;
+    int k = start;
+
+    int temp[100];
+
+    while(i <= mid && j <= end)
+    {
+        if(array[i] < array[j])
+        {
+            temp[k++] = array[i++];
+        }
+        else
+        {
+            temp[k++] = array[j++];
+        }
+    }
+
+    while(i <= mid)
+    {
+        temp[k++] = array[i++];
+    }
+
+    while(j <= end)
+    {
+        temp[k++] = array[j++];
+    }
+
+    for(int i = start; i <= end; i++)
+    {
+        array[i] = temp[i];
+    }
+
+}
+
+void mergeSort(int *array, int start, int end)
+{
+    if(start == end)
+        return;
+
+    int mid = (start+end)/2;
+
+    mergeSort(array, start, mid);
+    mergeSort(array, mid+1, end);
+
+    merge(array, start, end);
+}
+
+void knapsackSolve(int *cost, int *weight, int numItems, int maxWeight)
+{
+    int *costWeightRatio = malloc(sizeof(int) * numItems);
+
+    for(int i = 0; i < numItems; i++)
+    {
+        costWeightRatio[i] = cost[i]/weight[i];
+    }
+
+    int *sortedRatio = malloc(sizeof(int) * numItems);
+
+    for(int i = 0; i < numItems; i++)
+    {
+        sortedRatio[i] = costWeightRatio[i];
+    }
+
+
+
+
+
+}
+
 
 
 int main()
@@ -32,6 +121,9 @@ int main()
     generateItems(cost, weight, numItems);
     printArray(cost, numItems);
     printArray(weight, numItems);
+
+    mergeSort(cost, 0, numItems-1);
+    printArray(cost, numItems);
 
     return 0;
 }
